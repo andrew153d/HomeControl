a7c41b5834a5727fb1374878faabe95e37437a94
andrew153d <andrewwisnergrow@gmail.com>
2023-01-03 21:41:43 -0500

    fixed mobile version, desktop needs work

--- HomeApp.py
+++ HomeApp.py
@@ -9,7 +9,6 @@ MQTT_ADDRESS = '192.168.12.217'
 MQTT_USER = 'cdavid'
 MQTT_PASSWORD = 'cdavid'
 MQTT_TOPIC = 'home/office/deskLights'
-MQTT_IF_TOPIC = 'home/internet_fan'
 MQTT_CLIENTS = '$SYS/broker/clients/total'
 
 
@@ -31,7 +30,28 @@ powerSts = "OFF"
 num_connected_devices = 1
 
 
-mqtt_client = mqtt.Client()
+mqtt_client = mqtt.Client("pi")
+mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
+
+# Define a message callback function
+def on_message(client, userdata, message):
+	global num_connected_devices
+	topic = message.topic
+	payload = message.payload.decode()
+	print("Received message on topic '{}': {}".format(topic, payload))
+	if (topic == MQTT_CLIENTS):
+		num_connected_devices = payload
+
+
+
+mqtt_client.on_message = on_message
+
+mqtt_client.connect(MQTT_ADDRESS, 1883)
+
+mqtt_client.subscribe(MQTT_TOPIC)
+mqtt_client.subscribe(MQTT_CLIENTS)
+
+mqtt_client.loop_start()
 
 form_data = {
 'field' : 'field_value',
@@ -50,25 +70,10 @@ templateData = {
 		
 	}
 
-def on_connect(client, userdata, flags, rc):
-	""" The callback for when the client receives a CONNACK response from the server."""
-	print('Connected with result code ' + str(rc))
-	client.subscribe(MQTT_TOPIC)
-	client.subscribe(MQTT_IF_TOPIC)
-	client.subscribe(MQTT_CLIENTS)
 
-
-def on_message(client, userdata, msg):
-    """The callback for when a PUBLISH message is received from the server."""
-    print(msg.topic + ' ' + str(msg.payload))
 
 def sendColors():
 	global powerSts
-	
-		
-	mqtt_client.connect(MQTT_ADDRESS, 1883)
-	mqtt_client.loop_start()
-	mqtt_client.subscribe(MQTT_TOPIC)
 	
 	def getStr(input):
 		msg = ''
@@ -89,7 +94,7 @@ def sendColors():
 	else:
 		mqtt_client.publish(MQTT_TOPIC, msg)
 	
-	mqtt_client.loop_stop()
+	
 
 @app.route('/data/<color>', methods = ['POST'])
 def handleColorInput(color):
@@ -150,7 +155,15 @@ def get_template():
 	}
 	return templateData
 
-def extractColor(form_data):
+def extractName(form_data):
+
+	try:
+		output =  next(iter(form_data))
+	except(StopIteration):
+		output = ''
+	return output
+
+def handleColor(form_data):
 	global redValu
 	global grnValu
 	global bluValu
@@ -170,10 +183,39 @@ def extractColor(form_data):
 	except(StopIteration):
 		pass
 
+def handlePresets(form_data):
+	global redValu
+	global grnValu
+	global bluValu
+	preset = form_data['light']
+	print(preset)
+	if(preset == 'optionA'):
+		redValu = '100'
+		grnValu = '100'
+		bluValu = '100'
+	elif(preset == 'optionB'):
+		redValu = '0'
+		grnValu = '255'
+		bluValu = '255'
+	elif(preset == 'optionC'):
+		redValu = '30'
+		grnValu = '255'
+		bluValu = '30'
+	elif(preset == 'optionD'):
+		redValu = '255'
+		grnValu = '0'
+		bluValu = '255'
+	sendColors()
+
 
 @app.route("/", methods = ['POST', 'GET'])
 def index():
-	extractColor(request.form)
+	name = extractName(request.form)
+	print(name)
+	if (name == 'rfield' or name == 'gfield' or name == 'bfield'):
+		handleColor(request.form)
+	elif(name == 'light'):
+		handlePresets(request.form)	
 	
 	template_data = get_template()
 	return render_template('index.html', **template_data)
@@ -193,7 +235,8 @@ def action(deviceName, action):
 		if action == 'conn':
 			if connSts == "Disconnected":
 				connSts = "Connected"
-				mqtt_client.connect(MQTT_ADDRESS, 1883)
+				
+
 			elif connSts == "Connected":
 				connSts = "Disconnected"
 	if deviceName == 'powerSts':
@@ -212,9 +255,7 @@ def action(deviceName, action):
 	return render_template('index.html', **template_data)
 
 def main():
-    mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
-    mqtt_client.on_connect = on_connect
-    mqtt_client.on_message = on_message
+   pass 
 
 if __name__ == "__main__":
     main()
